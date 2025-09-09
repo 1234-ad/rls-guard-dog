4b8568f07cc10084568ea30a84d4bb221108c029
Aditya <97450298+1234-ad@users.noreply.github.com>
2025-09-09 14:05:05 +0530

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['localhost'],
+  },
+  env: {
+    CUSTOM_KEY: 'rls-guard-dog',
+  },
+}
+
+module.exports = nextConfig
